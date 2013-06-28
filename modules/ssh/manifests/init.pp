fb057c059757845dc5de719708d0a7792823a141
Jay Choksi <jchoksi@cisco.com>
2013-06-28 01:19:55 -0400

    Add SSH Class for Node2

--- modules/ssh/manifests/init.pp
+++ modules/ssh/manifests/init.pp
@@ -1,26 +1,13 @@
-class httpd {
-	package { 'httpd':
-	ensure => installed,
-	}
+class ssh {
 
-	service { 'httpd':
+	service { 'sshd':
 	ensure => running,
-	require => Package['httpd'],
 	enable => true,
 	}
 
-	file { '/var/www/html/cat-pictures/index.html':
-	source => 'puppet:///modules/httpd/index.html',
-	}
-
-	file { '/etc/httpd/conf.d/cat-pictures.conf':
-	source => 'puppet:///modules/httpd/cat-pictures.conf',
-	notify => Service['httpd'],
-	}
-	
-	file { '/etc/httpd/conf/httpd.conf':
-	source => 'puppet:///modules/httpd/httpd.conf',
-	notify => Service['httpd'],
+	file { '/etc/ssh/sshd_config':
+	source => 'puppet:///modules/ssh/sshd_config',
+	notify => Service['sshd'],
 	}
 	
 }
