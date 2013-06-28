bed6f6407e9abd2732b0f2d117bed3970333a2f9
Jay Choksi <jchoksi@cisco.com>
2013-06-28 01:29:23 -0400

    Update Openssh and manae ssh permission for Node2

--- modules/ssh/manifests/init.pp
+++ modules/ssh/manifests/init.pp
@@ -1,12 +1,18 @@
 class ssh {
-
+	package { 'openssh-server':
+        ensure => latest
+	} 
 	service { 'sshd':
 	ensure => running,
 	enable => true,
+	require   => Package['openssh-server'],
 	}
 
 	file { '/etc/ssh/sshd_config':
 	source => 'puppet:///modules/ssh/sshd_config',
+	owner => root,
+	group   => root,
+        mode    => 644,
 	notify => Service['sshd'],
 	}
 	
