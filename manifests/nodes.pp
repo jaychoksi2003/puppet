2488bca95059b57db4fa5dbbcbb1d8ca235d3eed
Jay Choksi <jchoksi@cisco.com>
2013-06-28 04:18:24 -0400

    Jboss user added on server

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,7 +1,16 @@
 node 'pserver' {
-include nginx
-include httpd
-include git
+	include nginx
+	include httpd
+	include git
+
+	user { 'jboss':
+	ensure => present,
+	comment => 'Jboss Users manage by puppet',
+	home => '/opt/jboss',
+	managehome => true,
+	}
+
+
 }
 node 'pclient' {
 include nginx
