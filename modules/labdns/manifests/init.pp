74ed62c2434f997d592b7e3beb566dfa50102e45
Jay Choksi <jchoksi@cisco.com>
2013-07-02 02:59:45 -0400

    LAB DNS Class added

--- modules/labdns/manifests/init.pp
+++ modules/labdns/manifests/init.pp
@@ -1,5 +1,5 @@
 class labdns {
-        exec {
+        exec { 'Manage DNS':
 	command => 'echo nameserver 10.122.90.11 > /etc/resolv.conf',
         unless => 'grep 10.122.90.11 /etc/resolv.conf',
         }
