7574c9c978cba31946df03ae6f3569cd59fd2e1c
Jay Choksi <jchoksi@cisco.com>
2013-07-03 06:24:43 -0400

    Resolve.conf

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -65,20 +65,7 @@ node 'pclient' {
         minute => '*/5',
 	user => 'git'
         }
-################################
-
-
-	file { '/etc/httpd/conf.d/cat-pictures.conf':
-	ensure => absent,
-	}
-
-
-#$site_name = 'pclient'
-#$site_domain = 'pclient.vms.spastp.cisco.com'
-#        file { "/etc/httpd/conf.d/${site_name}.conf":
-#        content => template('httpd/vhost.conf.erb'),
-#        notify => Service['httpd'],
-#        }
+################################ For Http ##################
 
 	file { "/var/www/html/${site_name}/index.html":
         source => 'puppet:///modules/httpd/index.html',
@@ -87,7 +74,7 @@ node 'pclient' {
 	httpd::website { 'pclient':
         site_domain => 'pclient.vms.spastp.cisco.com',
         }
-
+########################################################
 
 
 }
