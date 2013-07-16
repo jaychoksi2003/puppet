c9811201eb02b205a8da084c45731d88eacbe19d
Jay Choksi <jchoksi@cisco.com>
2013-07-16 05:41:23 -0400

    Add nagiOs class

--- modules/nagioscron/manifests/init.pp
+++ modules/nagioscron/manifests/init.pp
@@ -3,7 +3,7 @@ class nagioscron {
 	cron { 'Git Pull':
 	command => '/usr/local/bin/pull-updates',
 	hour => '*',
-        minute => '*/5',
+        minute => '*/59',
         user => 'root',
 	environment =>  'MAILTO=jchoksi@cisco.com',
         }
