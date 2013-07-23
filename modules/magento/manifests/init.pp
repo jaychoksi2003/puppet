82ba595fba449fdbce65c715fb76f80d90fbf171
Jay Choksi <jchoksi@cisco.com>
2013-07-23 06:30:43 -0400

    magento class

---
+++ modules/magento/manifests/init.pp
@@ -0,0 +1,12 @@
+class magento( $db_username, $db_password, $version, $admin_username, $admin_password, $use_rewrites) {	
+
+	exec { "create-magentodb-db":
+        unless => "/usr/bin/mysql -uroot -p${mysql::root_password} magentodb",
+        command => "/usr/bin/mysqladmin -uroot -p${$mysql::root_password} create magentodb",
+        require => [Service["mysql"]]
+  }
+	exec { "grant-magentodb-db-all":
+        unless => "/usr/bin/mysql -u${db_username} -p${db_password} magentodb",
+        command => "/usr/bin/mysql -uroot -p${$mysql::root_password} -e \"grant all on *.* to magento@'%' identified by '${db_password}' WITH GRANT OPTION;\"",
+        require => [Service["mysql"], Exec["create-magentodb-db"]]
+  }
