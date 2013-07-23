302970aff467cf82fc1364aef25ef9e686b310e7
Jay Choksi <jchoksi@cisco.com>
2013-07-23 04:24:26 -0400

    php class

--- modules/php/manifests/init.pp
+++ modules/php/manifests/init.pp
@@ -1,6 +1,6 @@
 class php {
         package {
-                ["php", "php-cli", "php-common", "php-mysql", "php-curl", "php-gd", "php-intl", "php-mcrypt", "php-mbstring"]:
+                ["php", "php-cli", "php-common", "php-mysql", "php-gd", "php-intl", "php-mcrypt", "php-mbstring"]:
 #                version => "5.3.27-2.w5",
                 ensure => '5.3.27-2.w5',
         }
