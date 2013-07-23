35c66484f71884a821a7f2e33f0e4a935d5a09e7
Jay Choksi <jchoksi@cisco.com>
2013-07-23 03:48:02 -0400

    php class

--- modules/php/manifests/init.pp
+++ modules/php/manifests/init.pp
@@ -1,8 +1,8 @@
 class php {
         package {
                 ["php"]:
-                version        => "5.3.27-2.w5",
-                ensure => installed,
+#                version => "5.3.27-2.w5",
+                ensure => '5.3.27-2.w5',
         }
 }
 
