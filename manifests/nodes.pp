87860d53b612c68aee54506081e0d6bb000b3d9b
Jay Choksi <jchoksi@cisco.com>
2013-07-23 06:29:12 -0400

    magento class

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -70,7 +70,25 @@ node 'pclient' {
 	class { "mysql":
 	root_password => "root",
 	}
-	
+/**
+ * Magento config
+ */
+class { "magento":
+    /* magento version */
+    version        => "1.6.1.0",
+
+    /* magento database settings */
+    db_username    => "magento",
+    db_password    => "magento",
+
+    /* magento admin user */
+    admin_username => "admin",
+    admin_password => "123123abc",
+
+    /* "yes|no */
+    use_rewrites   => "no",
+}
+#################################################################################################	
 	$nagios_server = 'pserver'
 	include nagiosclient
 
