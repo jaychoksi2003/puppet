cbd5abf8f3fcd9f19d661248fe71ca2c73f49acd
Jay Choksi <jchoksi@cisco.com>
2013-07-23 04:28:27 -0400

    php class

--- modules/mysql/manifests/init.pp
+++ modules/mysql/manifests/init.pp
@@ -15,12 +15,12 @@ class mysql( $root_password ) {
     exec { "Create magento user":
       unless => "/usr/bin/mysqladmin -umagento -pmagento status",
       command => "/usr/bin/mysql -uroot -p${root_password} -e \"CREATE USER magento@'%' IDENTIFIED BY 'magento'; Grant all on *.* TO magento@'%';\"",
-      require => Service["mysql"],
+      require => Service["mysqld"],
     }
 
-    service { "mysql":
+    service { "mysqld":
         require => [ Package["mysql-server"], Exec['Set MySQL server root password'] ],
-        #hasstatus => true,
+        hasstatus => true,
 	ensure => running,
     }	
 }
