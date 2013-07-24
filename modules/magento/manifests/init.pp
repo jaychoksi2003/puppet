deb0eb1c8490ef1c31d6d0e99ff317b63abc1107
Jay Choksi <jchoksi@cisco.com>
2013-07-24 03:44:25 -0400

    Add nagiOs class

--- modules/magento/manifests/init.pp
+++ modules/magento/manifests/init.pp
@@ -24,13 +24,14 @@ class magento( $db_username, $db_password, $version, $admin_username, $admin_pas
 	creates => "/tmp/enterprise-${version}.tar.gz",
   	}
 
-#	file { "${document_root}":
-#        ensure => "directory",
-#        mode => 755,
-#        owner => "apache",
-#        group  => "apache",
+	file { "${document_root}":
+        ensure => "directory",
+        mode => 755,
+        owner => "apache",
+        group  => "apache",
 #	recurse => "true",
-#        }
+	require => [Exec["untar-magento"]]
+        }
 
 	
 	exec { "untar-magento":
