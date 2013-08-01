d394059f280162fbce8ce40fdbeef6dbc81016be
root <root@pserver.vms.spastp.cisco.com>
2013-08-01 05:58:21 -0400

    Modfifying VMS  class

--- modules/vms/manifests/install.pp
+++ modules/vms/manifests/install.pp
@@ -2,7 +2,7 @@ class vms::install {
         package { [ 'rng-tools']:
         ensure => installed,
         }
-	if $role == "vms_installer" {
+	if $vms_role == "vms_installer" {
 
 	file { '/tmp/limits.conf':
         source => 'puppet:///modules/vms/limits.conf',
